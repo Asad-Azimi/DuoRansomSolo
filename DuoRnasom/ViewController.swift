18791eff3abef8a15d327a4dbaa3010730580fe1
Asad <asadullah@duolingo.com>
2025-06-14 22:32:27 -0400

    Initial Commit

---
+++ DuoRnasom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuoRnasom
+//
+//  Created by Asadullah Azimi on 6/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
